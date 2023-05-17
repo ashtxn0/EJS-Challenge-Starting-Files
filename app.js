a3460c310b76df7dcbdb08450f510cb2e5b5d0df
Ashton Areia <ashtonareia@outlook.com>
2023-05-16 23:52:06 -0400

    Challenges 1-3: add h1, p, header and footer to the home page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const homeStartingContent =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  "Bacon ipsum dolor amet short ribs ham hock picanha porchetta spare ribs meatloaf. Shoulder boudin swine hamburger sirloin leberkas buffalo tenderloin strip steak chislic corned beef pork belly. Shoulder andouille beef ribs, landjaeger tongue pork belly meatloaf. Biltong ground round buffalo, pork loin burgdoggen chislic picanha pancetta chuck doner sirloin.";
 const aboutContent =
   "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
 const contactContent =
@@ -15,6 +15,10 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", function(req,res){
+  res.render("home", {homeDescription: homeStartingContent});
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
